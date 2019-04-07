5761ae64f0363d3e1ea92fe048784727e0153b44
Tin Dvorski <tin.dvorski@student.mev.hr>
2019-04-07 12:46:44 +0200

    Dio pdf-a

--- WebApplication7/Controllers/AkcijaController.cs
+++ WebApplication7/Controllers/AkcijaController.cs
@@ -1,10 +1,14 @@
 ﻿using Akcija_prodaja.BazaContext;
 using Akcija_prodaja.Models;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -202,6 +206,100 @@ namespace WebApplication7.Controllers
             }
 
             return RedirectToAction("PopisAkcija");
+        }
+
+
+        public FileResult CreatePdf()
+        {
+            MemoryStream workStream = new MemoryStream();
+            StringBuilder status = new StringBuilder("");
+            DateTime dTime = DateTime.Now;
+            //file name to be created   
+            string strPDFFileName = string.Format("SamplePdf" + dTime.ToString("ddMMyyyy") + "-" + ".pdf");
+            Document doc = new Document();
+            doc.SetMargins(0f, 0f, 0f, 0f);
+            //Create PDF Table with 5 columns  
+            PdfPTable tableLayout = new PdfPTable(5);
+            doc.SetMargins(0f, 0f, 0f, 0f);
+            //Create PDF Table  
+
+            //file will created in this path  
+            string strAttachment = Server.MapPath("~/Preuzimanja/" + strPDFFileName);
+
+
+            PdfWriter.GetInstance(doc, workStream).CloseStream = false;
+            doc.Open();
+
+            //Add Content to PDF   
+            doc.Add(Add_Content_To_PDF(tableLayout));
+
+            // Closing the document  
+            doc.Close();
+
+            byte[] byteInfo = workStream.ToArray();
+            workStream.Write(byteInfo, 0, byteInfo.Length);
+            workStream.Position = 0;
+
+
+            return File(workStream, "application/pdf", strPDFFileName);
+
+        }
+        protected PdfPTable Add_Content_To_PDF(PdfPTable tableLayout)
+        {
+
+            float[] headers = { 50, 24, 45, 35, 50 }; //Header Widths  
+            tableLayout.SetWidths(headers); //Set the pdf headers  
+            tableLayout.WidthPercentage = 100; //Set the PDF File witdh percentage  
+            tableLayout.HeaderRows = 1;
+            //Add Title to the PDF file at the top  
+
+            List<AkcijaModel> akcije = baza.Akcije.ToList<AkcijaModel>();
+
+
+
+            tableLayout.AddCell(new PdfPCell(new Phrase("Creating Pdf using ItextSharp", new Font(Font.FontFamily.HELVETICA, 8, 1, new iTextSharp.text.BaseColor(0, 0, 0)))) {
+                Colspan = 12, Border = 0, PaddingBottom = 5, HorizontalAlignment = Element.ALIGN_CENTER
+            });
+
+
+            ////Add header  
+            AddCellToHeader(tableLayout, "ID akcije");
+            AddCellToHeader(tableLayout, "Naziv akcije");
+            AddCellToHeader(tableLayout, "Datum početka");
+            AddCellToHeader(tableLayout, "Datum završetka");
+            AddCellToHeader(tableLayout, "Opis");
+
+            ////Add body  
+
+            foreach (var akc in akcije)
+            {
+
+                AddCellToBody(tableLayout, akc.id_akcija.ToString());
+                AddCellToBody(tableLayout, akc.naziv_akcija);
+                AddCellToBody(tableLayout, akc.datum_pocetka.ToString());
+                AddCellToBody(tableLayout, akc.datum_zavrsetka.ToString());
+                AddCellToBody(tableLayout, akc.opis);
+
+            }
+
+            return tableLayout;
+        }
+        private static void AddCellToHeader(PdfPTable tableLayout, string cellText)
+        {
+
+            tableLayout.AddCell(new PdfPCell(new Phrase(cellText, new Font(Font.FontFamily.HELVETICA, 8, 1, iTextSharp.text.BaseColor.YELLOW)))
+    {
+                HorizontalAlignment = Element.ALIGN_LEFT, Padding = 5, BackgroundColor = new iTextSharp.text.BaseColor(128, 0, 0)
+    });
+        }
+
+        // Method to add single cell to the body  
+        private static void AddCellToBody(PdfPTable tableLayout, string cellText)
+        {
+            tableLayout.AddCell(new PdfPCell(new Phrase(cellText, new Font(Font.FontFamily.HELVETICA, 8, 1, iTextSharp.text.BaseColor.BLACK)))
+             {
+                HorizontalAlignment = Element.ALIGN_LEFT, Padding = 5, BackgroundColor = new iTextSharp.text.BaseColor(255, 255, 255)
+    });
         }
     }
 }
